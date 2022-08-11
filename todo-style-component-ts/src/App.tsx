c1396ba01fb78717a5c3ecf0f4620326864aa8f1
vivek lokhande <vivekl@geekyants.com>
2022-08-11 17:35:59 +0530

    initial commit

---
+++ todo-style-component-ts/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+import Home from './Pages/Home';
+function App() {
+  return (
+    <div className='screen'>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-style-component-ts/src/store/action.ts
@@ -0,0 +1,4 @@
+export const actions = {
+  addTodo: 'ADD_TODO',
+  deleteTodo: 'DELETE_TODO',
+};
